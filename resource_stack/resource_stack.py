778e0cda824a5b229d522c80f321d04c5455f6e1
shashi7795 <skrishna203@gmail.com>
2023-07-16 01:28:20 +0530

    add commands in codepipeline file

--- resource_stack/resource_stack.py
+++ resource_stack/resource_stack.py
@@ -6,23 +6,31 @@ from aws_cdk import (
     aws_s3 as s3
 )
 from constructs import Construct
+from aws_cdk import aws_iam as iam
 
 
 class ResourceStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-        queue = sqs.Queue(
-            self, "AWSCDKCodePipelineappDemoQueue",
-            visibility_timeout=Duration.seconds(300),
-            queue_name="demo_queue"
+
+        # Define the Lambda execution role
+        lambda_execution_role = iam.Role(
+            self,
+            "LambdaExecutionRole",
+            assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"),
+            role_name="LambdaExecutionRole"
         )
+
+        # Attach necessary policies to the Lambda execution role
+        lambda_execution_role.add_managed_policy(
+            iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AWSLambdaBasicExecutionRole")
+        )
+
+
         function = function_lambda.Function(self,
                                             "shashi-demo",
                                             function_name="codepipeline_lambda",
                                             runtime=function_lambda.Runtime.PYTHON_3_9,
                                             code=function_lambda.Code.from_asset('./lambda_code_demo'),
-                                            handler="demo_lambda.lambda_handler")
-
-        bucket = s3.Bucket(self, "MyfirstBucket", versioned=True,
-                           bucket_name="shashi-demo",
-                           block_public_access=s3.BlockPublicAccess.BLOCK_ALL)
+                                            handler="demo_lambda.lambda_handler",
+                                            role=lambda_execution_role)
